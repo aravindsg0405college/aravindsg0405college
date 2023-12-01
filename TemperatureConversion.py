19df5d693a433162cefb12003202ca1d74b97ccf
aravindsg0405college <152474562+aravindsg0405college@users.noreply.github.com>
2023-11-30 23:31:06 -0800

    Add files via upload
    This python program takes an input in degree celcius and converts it into fahrenheit and displays it

---
+++ TemperatureConversion.py
@@ -0,0 +1,29 @@
+
+'''
+   Description: Converting celsius to fahrenheit
+   Output: See Below
+   Known bugs: none
+'''
+
+TempInCelsius = eval(input("Enter a degree in celsius"))
+TempInFahrenheit = (9/5) * TempInCelsius + 32
+print(str(TempInCelsius) + " Celsius is " + str(TempInFahrenheit) + "Fahrenheit")
+
+'''
+Output
+ Enter a degree in celsius 43
+ 43 Celsius is 109.4Fahrenheit
+ 
+ Enter a degree in celsius 56
+ 56 Celsius is 132.8Fahrenheit
+  
+'''
+
+
+
+
+
+
+
+
+
